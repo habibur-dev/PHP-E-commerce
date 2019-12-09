a979712ef808403ed76980aefbd02fa9d49a26a4
Habibur <habibur500257@gmail.com>
2019-12-10 01:21:02 +0600

    Added customer files

---
+++ customer/includes/footer.php
@@ -0,0 +1,134 @@
+<div id="footer"><!-- #footer Begin -->
+
+	<div class="container"><!-- container Begin -->
+
+		<div class="row"><!-- row Begin -->
+
+			<div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+
+				<h4>Pages</h4>
+
+				<ul><!-- ul Begin -->
+					<li><a href="cart.php">Shopping Cart</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+					<li><a href="shop.php">Shop</a></li>
+					<li><a href="checkout.php">My Account</a></li>
+				</ul><!-- ul Finish -->
+
+				<hr>
+
+				<h4>User Section</h4>
+
+				<ul><!-- ul Begin -->
+					<li><a href="checkout.php">Login</a></li>
+					<li><a href="customer_register.php">Register</a></li>
+				</ul><!-- ul Finish -->
+
+				<hr class="hidden-md hidden-lg hidden-sm">
+				
+			</div><!-- col-sm-6 col-md-3 Finish -->
+
+			<div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+
+				<h4>Top Products Categories</h4>
+
+				<ul><!-- ul Begin -->
+					<li><a href="#">Jackets</a></li>
+					<li><a href="#">Assessories</a></li>
+					<li><a href="#">Coats</a></li>
+					<li><a href="#">Shoes</a></li>
+					<li><a href="#">T-Shirts</a></li>
+				</ul><!-- ul Finish -->
+
+				<hr class="hidden-md hidden-lg">
+				
+			</div><!-- col-sm-6 col-md-3 Finish -->
+
+			<div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+
+				<h4>Find Us:</h4>
+
+				<p><!-- p Begin -->
+
+					<strong>PetShop Inc.</strong>
+					<br>Rangpur Sadar
+					<br>Rangpur
+					<br>+8801 763 007 602
+					<br>support@petshop.com
+					<br><strong>PetShop</strong>
+					
+				</p><!-- p Finish -->
+
+				<a href="contact.php">Check Our Contact</a>
+
+				<hr class="hidden-md hidden-lg">
+				
+			</div><!-- col-sm-6 col-md-3 Finish -->
+
+			<div class="col-sm-6 col-md-3"><!-- col-sm-6 col-md-3 Begin -->
+
+				<h4>Get The News</h4>
+
+				<p class="text-muted">
+
+					Don't miss our latest update products.
+					
+				</p>
+
+				<form action="" method="post"><!-- form Begin -->
+
+					<div class="input-group"><!-- input-group Begin -->
+
+						<input type="text" name="email" class="form-control">
+
+						<span class="input-group-btn"><!-- input-group-btn Begin -->
+
+							<input type="submit" class="btn btn-default" value="Subscribe">
+							
+						</span><!-- input-group-btn Finish -->
+						
+					</div><!-- input-group Finish -->
+					
+				</form><!-- form Finish -->
+
+				<hr>
+
+				<h4>Keep In Touch</h4>
+
+				<p class="social">
+
+					<a href="#" class="fa fa-facebook"></a>
+					<a href="#" class="fa fa-twitter"></a>
+					<a href="#" class="fa fa-instagram"></a>
+					<a href="#" class="fa fa-google-plus"></a>
+					<a href="#" class="fa fa-envelope"></a>
+
+				</p>
+				
+			</div><!-- col-sm-6 col-md-3 Finish -->
+			
+		</div><!-- row Finish -->
+		
+	</div><!-- container Finish -->
+	
+</div><!-- #footer Finish -->
+
+<div id="copyright"><!-- #copyright Begin -->
+
+	<div class="container"><!-- container Begin -->
+
+		<div class="col-md-6"><!-- col-md-6 Begin -->
+
+			<p class="pull-left">&copy; 2019 PetShop Ltd. All Rights Reserved</p>
+			
+		</div><!-- col-md-6 Finish -->
+
+		<div class="col-md-6"><!-- col-md-6 Begin -->
+
+			<p class="pull-right">Developed by: <a href="#">Monstar</a></p>
+			
+		</div><!-- col-md-6 Finish -->
+		
+	</div><!-- container Finish -->
+	
+</div><!-- #copyright Finish -->
